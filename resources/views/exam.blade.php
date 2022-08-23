e1bc4907e850f590047d4e8b8d63bd5e8a57de08
Hung Nguyen <hungnguyenpisces@gmail.com>
2022-08-23 15:53:47 +0700

    initial repo

---
+++ resources/views/exam.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXAM</title>
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+
+</head>
+
+<body class="container">
+
+    <section class="mt-5">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1>Laravel EXAM</h1>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section>
+        <h3>Student Info</h3>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Student code</th>
+                            <th>Student ID</th>
+                            <th>Class</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Nguyễn Văn Hưng</td>
+                            <td>hungnvth2104019@fpt.edu.vn</td>
+                            <td>0973772755</td>
+                            <td>TH2104019</td>
+                            <td>Student1316992</td>
+                            <td>T2108E</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </section>
+    <section class="mt-5">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h2>PRACTICE</h2>
+            </div>
+        </div>
+    </section>
+    <hr>
+
+    <section>
+        <h3>Create</h3>
+    </section>
+
+    <section>
+        <h3>Read</h3>
+    </section>
+
+    <section>
+        <h3>Update</h3>
+    </section>
+
+    <section>
+        <h3>Delete</h3>
+    </section>
+
+    <section>
+        <!-- Modal create -->
+
+    </section>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
